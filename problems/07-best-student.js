1ff216da79093b7f1cc5743a8d4e588903ebd083
etkndr <103692901+etkndr@users.noreply.github.com>
2022-07-16 17:22:48 -0400

    07

--- problems/07-best-student.js
+++ problems/07-best-student.js
@@ -16,9 +16,32 @@
     
 */
 
+function bestStudent(...student) {
+    let numArr = []
+    let nameArr = []
+    let topStudent = ""
 
+    student.forEach(function(obj) {
+        numArr.push(obj.points)
+        nameArr.push(obj.name)
+    })
 
+    let highest = Math.max(...numArr)
+    for (let i = 0; i < numArr.length; i++) {
+        if (numArr[i] === highest) {
+            topStudent = nameArr[i]
+        }
+    }
+    return topStudent
+}
 
+const student1 = { name: "Geoff", points: 50 };
+const student2 = { name: "Justin", points: 1000 };
+const student3 = { name: "Tom", points: 568 };
+const student4 = { name: "Emily", points: 700 };
+const student5 = { name: "Corina", points: 750 };
+
+console.log(bestStudent(student1, student2, student3, student4, student5)) // returns "Justin"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
     module.exports = bestStudent;
