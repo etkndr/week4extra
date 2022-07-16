aff574542d5d85881e4c22614f9825519f93d901
etkndr <103692901+etkndr@users.noreply.github.com>
2022-07-16 16:06:34 -0400

    04

--- problems/04-all-the-marbles.js
+++ problems/04-all-the-marbles.js
@@ -14,6 +14,24 @@ All the marbles:
 */
 
 
+function allTheMarbles(...marble) {
+    let sum = 0
+    for (let i = 0; i < marble.length; i++) {
+        let obj = marble[i]
+        sum += obj.weight
+    }
+    return sum
+}
+
+const marble1 = { color: 'red', weight: 20 };
+const marble2 = { color: 'purple', weight: 100 };
+const marble3 = { color: 'blue', weight: 40 };
+
+console.log(allTheMarbles(marble1, marble2)) // prints 120;
+console.log(allTheMarbles(marble1, marble2, marble3)) // prints 160;
+console.log(allTheMarbles()) // prints 0;
+
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
