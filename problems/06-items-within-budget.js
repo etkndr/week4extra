0abc8bde3df12cf4bdf8e927899bedfd3cb006e9
etkndr <103692901+etkndr@users.noreply.github.com>
2022-07-16 17:04:27 -0400

    06

--- problems/06-items-within-budget.js
+++ problems/06-items-within-budget.js
@@ -18,7 +18,28 @@
     console.log(itemsWithinBudget(200, items2)) // prints ['omakase bento box', 'medicine']
 */
 
+let itemsWithinBudget = (budget, items) => {
+    let arr = []
+    items.forEach(function (item) {
+        if (item.price < budget) {
+            arr.push(item.name)
+        }
+    })
+    return arr
+}
 
+const items1 = [{name: 'iPhone', price: 700},
+{name: 'laptop', price: 2000},
+{name: 'guitar', price: 300},
+{name: 'camera', price: 1200}
+]
+const items2 = [ {name: 'online classes', price: 1000},
+  {name: 'omakase bento box', price: 100},
+  {name: 'medicine', price: 50}
+]
+
+console.log(itemsWithinBudget(1000, items1)) // prints ['iPhone', 'guitar'] 
+console.log(itemsWithinBudget(200, items2)) // prints ['omakase bento box', 'medicine']
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
